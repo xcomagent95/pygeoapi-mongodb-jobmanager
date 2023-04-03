6b9459aa8aefc5e090575903c2c6b5dae535cd34
Alexander Pilz <51150328+xcomagent95@users.noreply.github.com>
2023-04-03 11:15:33 +0200

    changes

--- mongodb_.py
+++ mongodb_.py
@@ -1,15 +1,38 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Feb 24 14:31:04 2023
-
-@author: Alexander Pilz for 52°North Spatial Information Research GmbH
-@contact: info@52north.org or a.pilz@52north.org
-"""
-from pygeoapi.process.manager.base import BaseManager
-from pymongo import MongoClient
-import traceback
+# =================================================================
+#
+# Authors: Alexander Pilz <a.pilz@52north.org>
+#
+# Copyright (c) 2023 Alexander Pilz
+#
+# Permission is hereby granted, free of charge, to any person
+# obtaining a copy of this software and associated documentation
+# files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use,
+# copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the
+# Software is furnished to do so, subject to the following
+# conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+# WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+#
+# =================================================================
 import json
 import logging
+import traceback
+
+from pymongo import MongoClient
+
+from pygeoapi.process.manager.base import BaseManager
 
 LOGGER = logging.getLogger(__name__)
 
@@ -27,7 +50,8 @@ class MongoDBManager(BaseManager):
             return True
         except Exception:
             self.destroy()
-            LOGGER.error("JOBMANAGER - connect error", exc_info=(traceback))
+            LOGGER.error("JOBMANAGER - connect error",
+                         exc_info=(traceback))
             return False
 
     def destroy(self):
@@ -37,7 +61,8 @@ class MongoDBManager(BaseManager):
             return True
         except Exception:
             self.destroy()
-            LOGGER.error("JOBMANAGER - destroy error", exc_info=(traceback))
+            LOGGER.error("JOBMANAGER - destroy error",
+                         exc_info=(traceback))
             return False
 
     def get_jobs(self, status=None):
@@ -52,7 +77,8 @@ class MongoDBManager(BaseManager):
             LOGGER.info("JOBMANAGER - MongoDB jobs queried")
             return jobs
         except Exception:
-            LOGGER.error("JOBMANAGER - get_jobs error", exc_info=(traceback))
+            LOGGER.error("JOBMANAGER - get_jobs error",
+                         exc_info=(traceback))
             return False
 
     def add_job(self, job_metadata):
@@ -64,7 +90,8 @@ class MongoDBManager(BaseManager):
             LOGGER.info("JOBMANAGER - MongoDB job added")
             return doc_id
         except Exception:
-            LOGGER.error("JOBMANAGER - add_job error", exc_info=(traceback))
+            LOGGER.error("JOBMANAGER - add_job error",
+                         exc_info=(traceback))
             return False
 
     def update_job(self, job_id, update_dict):
@@ -126,4 +153,4 @@ class MongoDBManager(BaseManager):
             return False
 
     def __repr__(self):
-        return '<MongoDBManager> {}'.format(self.name)
+        return f'<MongoDBManager> {self.name}'
